e11ed7ec08db669f43c819d32d683e2be447fbf3
monaqa <cmonaqa@gmail.com>
2021-12-17 09:19:17 +0900

    update deps
    - mainly to fix bug in std library (https://github.com/denoland/deno_std/pull/1676)

--- denops/dial/deps.ts
+++ denops/dial/deps.ts
@@ -1,6 +1,6 @@
-export type { Denops } from "https://deno.land/x/denops_std@v1.0.1/mod.ts";
-export * from "https://deno.land/x/denops_std@v1.0.1/helper/mod.ts";
-export * from "https://deno.land/x/denops_std@v1.0.1/variable/mod.ts";
-export * as fn from "https://deno.land/x/denops_std@v1.0.1/function/mod.ts";
-export * from "https://deno.land/x/unknownutil@v1.1.0/mod.ts";
-export * from "https://deno.land/std@0.103.0/datetime/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v2.2.0/mod.ts";
+export * from "https://deno.land/x/denops_std@v2.2.0/helper/mod.ts";
+export * from "https://deno.land/x/denops_std@v2.2.0/variable/mod.ts";
+export * as fn from "https://deno.land/x/denops_std@v2.2.0/function/mod.ts";
+export * from "https://deno.land/x/unknownutil@v1.1.4/mod.ts";
+export * from "https://deno.land/std@0.118.0/datetime/mod.ts";
